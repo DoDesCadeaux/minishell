a5969643c31c0e967d87ed0f335fda4ea93f9559
tverdood <tverdood@student.s19.be>
2022-08-11 15:41:10 +0200

    dossier Pau

---
+++ Pau/clone_envp.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clone_envp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pamartin <pamartin@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/05 18:38:59 by pamartin          #+#    #+#             */
+/*   Updated: 2022/08/05 18:39:00 by pamartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+char	**add_var(char **matrix, int len_matrix, int len_nline, char *n_line)
+{
+	char	**new_matrix;
+	int		i;
+
+	if (len_matrix == 0)
+		len_matrix++;
+	new_matrix = malloc(sizeof(char *) * (len_matrix + 1));
+	if (!new_matrix)
+		return (NULL);
+	i = 0;
+	while (matrix[i] && i <= len_matrix)
+	{
+		new_matrix[i] = malloc(sizeof(char) * ft_strlen(matrix[i]) + 1);
+		if (!new_matrix[i])
+			return (NULL);
+		ft_strcpy(new_matrix[i], matrix[i]);
+		i++;
+	}
+	new_matrix[i] = malloc(sizeof(char) * len_nline + 1);
+	if (!new_matrix[i])
+		return (NULL);
+	ft_strcpy(new_matrix[i], n_line);
+	new_matrix[i + 1] = 0;
+	ft_free_split(matrix);
+	return (new_matrix);
+}
+
+void	init_pwd(t_struct *data)
+{
+	char	*pwd_tmp;
+
+	pwd_tmp = NULL;
+	pwd_tmp = getcwd(pwd_tmp, 200);
+	//protection malloc
+	data->pwd = malloc(sizeof(char *) * 2 + 1);
+	//protection malloc
+	data->pwd[0] = ft_strdup(pwd_tmp);
+	//protection malloc
+	data->pwd[1] = ft_strdup(pwd_tmp);
+	//protection malloc
+	data->pwd[2] = 0;
+	free(pwd_tmp);
+}
+
+t_struct	*update_lvl(t_struct *data, char *lvl)
+{
+	char	*tmp;
+	int		new_lvl;
+
+	lvl = ft_strchr(lvl, 61);
+	if (is_numeric(lvl + 1))
+	{
+		new_lvl = ft_atoi(lvl + 1) + 1;
+		//warning: shell level (1501) too high, resetting to 1
+		if (new_lvl > 1000)
+			new_lvl = 1;
+		tmp = ft_itoa(new_lvl);
+		lvl = ft_strjoin("SHLVL=", tmp);
+		if (new_lvl == 1000)
+			data = export_env(data, "SHLVL=");
+		else
+			data = export_env(data, lvl);
+		free(lvl);
+		free(tmp);
+	}
+	return (data);
+}
+
+t_struct	*clone_env(char **env, t_struct *data)
+{
+	int		i;
+	char	*lvl;
+
+	data->envp = malloc(sizeof(char *) * len_split(env) + 1);
+	if (!data->envp)
+		return (NULL);
+	i = 0;
+	while (env[i])
+	{
+		data->envp[i] = ft_strdup(env[i]);
+		i++;
+	}
+	data->envp[i] = 0;
+	lvl = var_exist(data, "SHLVL");
+	if (lvl)
+		data = update_lvl(data, lvl);
+	else
+		data = export_env(data, "SHLVL=1");
+	init_pwd(data);
+	//Si old power existe il faut le free et adress a null
+	return (data);
+}
+
---
+++ Pau/envp.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   envp.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pamartin <pamartin@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/05 18:42:23 by pamartin          #+#    #+#             */
+/*   Updated: 2022/08/05 18:42:25 by pamartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+#include <errno.h>
+
+t_struct	*export_env(t_struct *data, char *export)
+{
+	//Fonctionnel en l'etat mais a revoir avec le parsing et le format des donnees
+	//il est possible d'export plusieurs variables en même temps
+	//Plusieurs possibilités:
+	//	->on rappel cette fonction dans une boucle qui pqsse tous les arg un a un
+	//	->tous est pris en compte dans cette fonction
+	int	i;
+
+	i = 0;
+	while (data->envp[i])
+	{
+		if (is_env_var(data, export, i))
+		{
+			if (ft_strncmp(data->envp[i], export, ft_strlen(data->envp[i])))
+			{
+				free(data->envp[i]);
+				data->envp[i] = malloc(sizeof(char) * ft_strlen(export) + 1);
+				if (!data->envp[i])
+					return (NULL);
+				ft_strcpy(data->envp[i], export);
+				return (data);
+			}
+		}
+		i++;
+	}
+	if (ft_strfchr(export, '=') > 1)
+		data->envp = add_var(data->envp, len_split(data->envp), ft_strlen(export), export);
+	return (data);
+}
+
+t_struct	*unset_env(t_struct *data, char *unset)
+{
+	//Fonctionnel en l'etat mais a revoir avec le parsing et le format des donnees
+	//il est possible d'export plusieurs variables en même temps
+	//Plusieurs possibilités:
+	//	->on rappel cette fonction dans une boucle qui pqsse tous les arg un a un
+	//	->tous est pris en compte dans cette fonction
+	int i;
+
+	i = 0;
+	while (data->envp[i])
+	{
+		if (!ft_strncmp(data->envp[i], unset, ft_strlen(unset)))
+		{
+			while(i < len_split(data->envp)-1)
+			{
+				free(data->envp[i]);
+				data->envp[i] = 0x0;
+				data->envp[i] = malloc(sizeof(char) * ft_strlen(data->envp[i + 1]));
+				ft_strcpy(data->envp[i], data->envp[i + 1]);
+				i++;
+			}
+			free(data->envp[i]);
+			data->envp[i] = 0x0;
+			return (data);
+		}
+		i++;
+	}
+	return (data);
+}
+
+void	env_builtin(t_struct *data)
+{
+	//Fonctionnel en l'etat
+	//Attention si il y a qqc apres env -> msg d'erreur
+	//Messages d'erreurs a vérifier
+	int	i;
+
+	i = 0;
+	while (data->envp[i])
+	{
+		printf("%s\n", data->envp[i]);
+		i++;
+	}
+}
