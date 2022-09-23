f6604ae82402749169e8859c947e14738e430253
tverdood <tverdood@student.s19.be>
2022-09-23 15:03:28 +0200

    norme avant d'attaquer le main.c

--- src/main.c
+++ src/main.c
@@ -49,7 +49,6 @@ int	main(int argc, char **argv, char **envp)
 		line = prompt();
 		if (syntax_errors(line))
 			continue ;
-//		line = remove_multi_space(line); //se fait dans le parsing
 		if (is_metachar(line[ft_strlen(line) - 1]))
 			line = get_full_pipe(line);
 		if (!is_pipe(line))
