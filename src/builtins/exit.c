2732c1892fc7188a5a46982a692a04f26f1c2661
Dorian Duraku <dduraku@student.s19.be>
2022-10-06 16:13:30 +0200

    Fix exit with non numeric arguments

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -16,7 +16,45 @@ static int	ft_norm(char *numbers)
 {
 	ft_error(msg("exit", numbers, MSG_NUM, 0), 2);
 	free(numbers);
-	return (2);
+	exit(2);
+}
+
+static void	get_argument(char *line, int i, int j, char *numbers)
+{
+	int	space_count;
+
+	space_count = 0;
+	while (line[i])
+	{
+		if (!ft_isdigit(line[i]) && space_count == 0)
+		{
+			if (line[i] == ' ')
+				space_count++;
+			i++;
+		}
+		else if (line[i] && space_count > 0)
+		{
+			numbers[j] = line[i];
+			j++;
+			i++;
+		}
+	}
+	numbers[j] = '\0';
+}
+
+static int	ft_argument_is_valid(char *numbers)
+{
+	int	i;
+
+	i = 0;
+	while (numbers[i])
+	{
+		if (ft_isdigit(numbers[i]))
+			i++;
+		else
+			return (0);
+	}
+	return (1);
 }
 
 static int	ft_return_nbr(char *line)
@@ -29,19 +67,10 @@ static int	ft_return_nbr(char *line)
 	i = 0;
 	j = 0;
 	numbers = malloc(sizeof(char) * ft_strlen(line) + 1);
-	while (line[i])
-	{
-		if (!ft_isdigit(line[i]))
-			i++;
-		else
-		{
-			numbers[j] = line[i];
-			j++;
-			i++;
-		}
-	}
-	numbers[j] = '\0';
-	if (ft_strlen(numbers) > 19 || ft_long_atoi(numbers) > 9223372036854775807)
+	get_argument(line, i, j, numbers);
+	if (ft_strlen(numbers) > 19
+		|| ft_long_atoi(numbers) > 9223372036854775807
+		|| !ft_argument_is_valid(numbers))
 		return (ft_norm(numbers));
 	tmp = ft_long_atoi(numbers);
 	free(numbers);
