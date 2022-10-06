752469ea2f073a5c3dae24c8e03b5f4c6752366a
Dorian Duraku <dduraku@student.s19.be>
2022-10-06 14:44:53 +0200

    bug $ Dorian last minute & Makefile

--- src/parsing/dollar.c
+++ src/parsing/dollar.c
@@ -29,7 +29,6 @@ static char	*replace_error_code(t_struct *data, char *line, int i)
 	dst = ft_strjoin(tmp, data->tmp_3);
 	free(tmp);
 	free(line);
-	ft_free_tmp(data);
 	return (dst);
 }
 
@@ -45,7 +44,7 @@ static char	*join_and_replace(t_struct *data, char *tmp1, char *rpl, char *tmp3)
 	if (tmp_dollar)
 	{
 		i = 0;
-		while (tmp_dollar[i] != '=' && tmp_dollar[i])
+		while (tmp_dollar[i] && tmp_dollar[i] != '=')
 			i++;
 		i++;
 		tmp_dollar = str_dup_parts(tmp_dollar, ft_strlen(tmp_dollar), i);
@@ -55,7 +54,8 @@ static char	*join_and_replace(t_struct *data, char *tmp1, char *rpl, char *tmp3)
 	else
 		dst = ft_strjoin(tmp1, tmp3);
 	free(tmp_dollar);
-	free(tmp);
+	if (tmp != NULL)
+		free(tmp);
 	return (dst);
 }
 
@@ -63,18 +63,21 @@ static char	*replace_or_erase(char *line, t_struct *data)
 {
 	char	*tmp;
 
+	tmp = NULL;
 	tmp = var_exist(data, data->tmp_2);
 	if (tmp)
 		line = join_and_replace(data, data->tmp_1, data->tmp_2, data->tmp_3);
 	else
 		line = ft_strjoin(data->tmp_1, data->tmp_3);
-	free(tmp);
+	if (tmp != NULL)
+		free(tmp);
 	return (line);
 }
 
 char	*replace_dollar(t_struct *data, char *line, int i)
 {
-	int	y;
+	int		y;
+	char	*tmp;
 
 	data->tmp_1 = str_dup_parts(line, i - 1, 0);
 	i++;
@@ -85,17 +88,17 @@ char	*replace_dollar(t_struct *data, char *line, int i)
 	while (line[i])
 		i++;
 	data->tmp_3 = str_dup_parts(line, i, y);
+	tmp = replace_or_erase(line, data);
 	free(line);
-	line = replace_or_erase(line, data);
-	ft_free_tmp(data);
-	return (line);
+	return (tmp);
 }
 
-char	*parsing_dollar(t_struct *data, char *line_pars, int i)
+char	*parsing_dollar(t_struct *data, char *line_to_pars, int i)
 {
-	if (is_error_code(line_pars, i))
-		line_pars = replace_error_code(data, line_pars, i);
-	if (line_pars[i] == '$')
-		line_pars = replace_dollar(data, line_pars, i);
-	return (line_pars);
+	if (is_error_code(line_to_pars, i))
+		line_to_pars = replace_error_code(data, line_to_pars, i);
+	if (line_to_pars[i] == '$')
+		line_to_pars = replace_dollar(data, line_to_pars, i);
+	ft_free_tmp(data);
+	return (line_to_pars);
 }
